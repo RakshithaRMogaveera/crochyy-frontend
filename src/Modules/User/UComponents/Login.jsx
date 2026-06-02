cc7dda571c160db4bd0cb17b03aa075df48d0908
RakshithaRMogaveera <rakshitharmogaveera18@gmail.com>
2026-06-02 20:06:58 +0530

    fix backend api urls

--- src/Modules/User/UComponents/Login.jsx
+++ src/Modules/User/UComponents/Login.jsx
@@ -20,7 +20,7 @@ export default function Login() {
   const handleLogin = () =>{
     console.log("login data:", login)
 
-    axios.post("http://crochyy-backend.onrender.com/user/Login", login)
+    axios.post("https://crochyy-backend.onrender.com/user/Login", login)
     .then((res)=>{
       console.log("login response:", res.data)
 
